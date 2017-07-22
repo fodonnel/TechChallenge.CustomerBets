633fbcb6bc4b2f0c9b363e1d6f345513eb0ef103
Fergal O'Donnell <fergal.odonnell@gmail.com>
2017-07-22 20:51:59 +1000

    Added table highlighters

--- src/TechChallenge.CustomerBets.Web/Models/BetViewModel.cs
+++ src/TechChallenge.CustomerBets.Web/Models/BetViewModel.cs
@@ -9,10 +9,17 @@ namespace TechChallenge.CustomerBets.Web.Models
     {
         public int CustomerId { get; set; }
         public string Name { get; set; }
+
         public decimal TotalReturnStake { get; set; }
+        public bool IsWinning => TotalReturnStake > 0;
+        public string TotalReturnStakeHighlight => IsWinning ? "success" : "danger";
 
         public int RaceId { get; set; }
+
         public decimal ReturnStake { get; set; }
+        public bool IsLargeWin => Won && ReturnStake > 200;
+        public string ReturnStakeHighlight => IsLargeWin ? "danger" : "";
+
         public bool Won { get; set; }
     }
 }
