009edfb69f214574fa486418079ca0a5e1e647a2
Fergal O'Donnell <fergal.odonnell@gmail.com>
2017-07-22 19:32:57 +1000

    wired up ninject

--- src/TechChallenge.CustomerBets.Web/Controllers/HomeController.cs
+++ src/TechChallenge.CustomerBets.Web/Controllers/HomeController.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TechChallenge.CustomerBets.Web.Services;
 
 namespace TechChallenge.CustomerBets.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IPingService _pingService;
+
+        public HomeController(IPingService pingService)
+        {
+            _pingService = pingService;
+        }
+
         public ActionResult Index()
         {
+            var message = _pingService.Ping();
+            ViewBag.Message = $"Ping service said: {message}";
+
             return View();
         }
 
---
+++ src/TechChallenge.CustomerBets.Web/Services/PingService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TechChallenge.CustomerBets.Web.Services
+{
+    public interface IPingService
+    {
+        string Ping();
+    }
+
+    public class PingService : IPingService
+    {
+        public string Ping()
+        {
+            return "Pong";
+        }
+    }
+}
